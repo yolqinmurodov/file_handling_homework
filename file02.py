52eb27c5410d16b827af1a9f4eb8995cf3a58778
yolqinmurodov <yolqinmurodov@gmail.com>
2026-04-28 14:59:12 +0500

    length of String

--- file02.py
+++ file02.py
@@ -1,10 +1,13 @@
 def main(data:str):
-    """
-    The data is from the file. Return number of characters in the file.
-    Args:
-        data: str
-    Returns:
-        int: return answer
-    """
+     """
+     The data is from the file. Return number of characters in the file.
+     Args:
+         data: str
+     Returns:
+         int: return answer
+     """
 
 # Read data from file
+f = open("data/data02.txt", "r")
+data = f.read()
+print(len(data))
