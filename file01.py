3a4c4c49c3ec5ee1d7f06e6c204a477cb7e2dc5c
yolqinmurodov <yolqinmurodov@gmail.com>
2026-04-28 14:04:35 +0500

    String to List

--- file01.py
+++ file01.py
@@ -1,10 +1,14 @@
-def main(data:str):
-    """
-    The data is from the file. Return data as a list type.
-    Args:
-        data: str
-    Returns:
-        list: return answer
-    """
-
+def file01():
+#     """
+#     The data is from the file. Return data as a list type.
+#     Args:
+#         data: str
+#     Returns:
+#         list: return answer
+#     """
+    f = open("data/data01.txt", "r")
+    data = f.read()
+    list1 = data.split(",")
+    return list1
+print(file01())    
 # Read data from file
